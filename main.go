5ca8b410b5098f1995eb46de4704ca439a95a8fe
hrt014 <alz1111@163.com>
2022-01-28 14:08:30 +0800

    hello init

---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2022-01-28 01:13:07
+ * @LastEditors: Szang
+ * @LastEditTime: 2022-01-28 13:59:59
+ * @FilePath: \hello\main.go
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello everybody! Im pocky, glad meet 2u!")
+}
